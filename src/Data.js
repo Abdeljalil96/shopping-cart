090fe2d14cb400fd4597a5995039732438092c05
abdeljalil96 <abdeljalilait996@gmail.com>
2023-03-17 16:15:22 +0100

    shopping cart

---
+++ src/Data.js
@@ -0,0 +1,29 @@
+let shopItemsData = [
+    {
+    id :"casual",
+    name :"Casual Shirt",
+    price :45,
+    desc : "Lorem ipsum dolor sit amet consecteur adipisicing",
+    img : "images/img-1.jpg"
+},
+{
+    id :"office",
+    name :"Classic Shirt",
+    price :100,
+    desc : "Lorem ipsum dolor sit amet consecteur adipisicing",
+    img : "images/img-2.jpg"
+},
+{
+    id :"shirt",
+    name :" Shirt",
+    price :25,
+    desc : "Lorem ipsum dolor sit amet consecteur adipisicing",
+    img : "images/img-3.jpg"
+},
+{
+    id :"suit",
+    name :"Mens Suit",
+    price :300,
+    desc : "Lorem ipsum dolor sit amet consecteur adipisicing",
+    img : "images/img-4.jpg"
+}];
